bac1f40d29d38b96eaf898696bcc780e786a600e
Aleksander Gorecki <alegorecki@gmail.com>
2020-01-04 19:25:03 +0100

    first commit

---
+++ mysite/polls/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField('date_published')
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300)
+    votes = models.IntegerField(default=0)
---
+++ mysite/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Django_Nauka\mysite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
