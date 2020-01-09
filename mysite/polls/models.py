1bbc0d6ea3770824e38776f8ccda4cc52903cf06
Aleksander Gorecki <alegorecki@gmail.com>
2020-01-09 23:18:15 +0100

    48:54

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,8 +4,10 @@ class Question(models.Model):
     question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField('date_published')
     def __str__(self):
-        return self.question_text+str(self.pub_date)
+        return self.question_text
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=300)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
