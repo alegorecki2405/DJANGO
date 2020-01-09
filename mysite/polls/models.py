8ab3c36e0edeefbaae6d342bb16338db748b4061
Aleksander Gorecki <alegorecki@gmail.com>
2020-01-09 23:12:27 +0100

    wow nie wiedziałem ze tak sie da xd

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField('date_published')
+    def __str__(self):
+        return question_text+str(pub_date)
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=300)
