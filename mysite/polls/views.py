53a2a0d157966ce502db7b3bb3c90475437fd172
Aleksander Gorecki <alegorecki@gmail.com>
2020-01-27 21:40:42 +0100

    no no dynamiczne urlsy nie powiem sztos 1:54:55

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 
@@ -14,11 +14,9 @@ def index(request):
     return render(request, "polls/index.html" , context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+
 
 
 
